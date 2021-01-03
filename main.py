45906273e70b44a046828ac953a2fa8779941786
jeanc <jeancamlap@gmail.com>
2021-01-03 21:49:48 +0100

    new file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
 from math import *
 from random import *
-import time
 
 
 def wordToNumber(chaine):
---
+++ newEssai.py
@@ -0,0 +1,10 @@
+
+def letterToNumber(lettre):
+    return ord(lettre)-65
+
+def wordToNumber(chaine):
+    return chaine
+
+
+
+print(letterToNumber("B"))
