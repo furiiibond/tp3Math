f5a389bc947601c1c61ef205dec3076a993f05c8
jeanc <jeancamlap@gmail.com>
2021-01-03 22:24:59 +0100

    wordtonumber but copy

--- newEssai.py
+++ newEssai.py
@@ -3,8 +3,20 @@ def letterToNumber(lettre):
     return ord(lettre)-65
 
 def wordToNumber(chaine):
-    return chaine
+    res = 0
+    tailleChaine = len(chaine)
+    if (tailleChaine == 1):
+        return letterToNumber(chaine)
+    else:
+        i = 0
+        for lettre in chaine:
+            if (i == tailleChaine - 1):
+                res += letterToNumber(lettre)
+            else:
+                res += (letterToNumber(lettre) + 1) * (26 ** (((tailleChaine - 1) - i)))
+            i = i + 1
+    return res
 
 
 
-print(letterToNumber("B"))
+print(wordToNumber("AAA"))
